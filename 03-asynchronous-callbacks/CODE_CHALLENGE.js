34a62fb64103d34c83a1a40fc8574c18ce086745
Steve Geluso <stevegeluso@gmail.com>
2018-02-10 10:18:46 -0800

    adding code challenge solution

--- 03-asynchronous-callbacks/CODE_CHALLENGE.js
+++ 03-asynchronous-callbacks/CODE_CHALLENGE.js
@@ -10,11 +10,23 @@
 // write a for loop that shifts all other values over to fill the now null space
 // manually adjust .length of the array to be one less
 function remove(arr, index) {
+  // erase the value at the given index.
+  arr[index] = null;
+
+  // Now shift everything over to fill the null left over.
+  for (let i = index; i <= arr.length - 1; i++) {
+    // replace each value of the array with the value to the right
+    arr[i] = arr[i + 1];
+  }
+
+  // adjust the length because now it's one smaller.
+  arr.length = arr.length - 1;
+
   return arr;
 }
 
 console.log("Expected:", [23, 75, 76])
-console.log(" Actual:", remove([23, 45, 75, 76], 1))
+console.log("  Actual:", remove([23, 45, 75, 76], 1))
 
 
 

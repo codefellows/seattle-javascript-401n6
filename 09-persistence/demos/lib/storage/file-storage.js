0acabe08c2798cb9a8837aabe1fb81dddf869bdd
Steve Geluso <stevegeluso@gmail.com>
2018-02-28 19:26:40 -0800

    creating tests for file storage

---
+++ 09-persistence/demos/lib/storage/file-storage.js
@@ -0,0 +1,70 @@
+const fs = require('fs');
+const Book = require('../../models/book');
+
+let BOOKS = {};
+let STORAGE_DIRECTORY = __dirname + '/savedfiles';
+
+function seed() {
+  BOOKS = {};
+  save(new Book("Dune", "Frank Herbert", 900 * 120));
+  save(new Book("Harry Potter", "JK Rowling", 900 * 98));
+  save(new Book("Hitchhiker's Guide", "Douglas Adams", 42 * 42 * 42));
+}
+
+function bookIDToFilename(id) {
+  return STORAGE_DIRECTORY + '/' + id + '.json';
+}
+
+function save(book) {
+  return new Promise((resolve, reject) => {
+    let filename = bookIDToFilename(book.id);
+    let data = JSON.stringify(book);
+    fs.writeFile(filename, data, (err) => {
+      resolve(book);
+    });
+  });
+}
+
+function get(id) {
+  return new Promise((resolve, reject) => {
+    let filename = bookIDToFilename(id);
+    fs.readFile(filename, (err, data) => {
+      let book = JSON.parse(data);
+      resolve(book); 
+    });
+  });
+}
+
+function getAll() {
+  return new Promise((resolve, reject) => {
+    fs.readdir(STORAGE_DIRECTORY, (err, files) => {
+      resolve(files); 
+    });
+  });
+}
+
+// watch out! 'delete' is a keyword in JavaScript. Get creative.
+function remove(id) {
+  return new Promise((resolve, reject) => {
+    get(id)
+    .then(book => {
+      let filename = bookIDToFilename(id);
+      fs.unlink(filename, (err) => {
+        resolve(book);
+      });
+    });
+  });
+}
+
+function removeAll() {
+  return getAll()
+  .then(files => {
+    let promises = files.map(file => {
+      let id = file.split(".json")[0];
+      return remove(id);
+    });
+    return Promise.all(promises);
+  });
+}
+
+module.exports = {seed, save, get, getAll, remove, removeAll};

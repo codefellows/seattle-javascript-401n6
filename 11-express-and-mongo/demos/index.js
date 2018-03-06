92c8bd0c80dadade1d4c7ad879a68dc31ea1f967
Steve Geluso <stevegeluso@gmail.com>
2018-03-05 18:47:42 -0800

    using body parser for json

--- 11-express-and-mongo/demos/index.js
+++ 11-express-and-mongo/demos/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
 const bookAPI = require('./api/bookapi');
+
+// Use body-parser to automatically parse JSON off incoming requests
+app.use(bodyParser.json());
 
 app.use('/api/books', bookAPI);
 

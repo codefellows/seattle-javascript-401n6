f9c4236a4de70df3e617b61a777c7c0a8d2aae67
Steve Geluso <stevegeluso@gmail.com>
2018-04-23 20:24:46 -0700

    using router

--- 28-routing-and-testing/lecture/routing-react/src/app.jsx
+++ 28-routing-and-testing/lecture/routing-react/src/app.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
 import Homepage from './components/homepage.jsx';
 import Contacts from './components/contacts.jsx';
 
@@ -13,11 +19,21 @@ class App extends React.Component {
   }
 
   render() {
-    return <div>
-      <h1>{this.state.title}</h1>
-      <Homepage />
-      <Contacts />
-    </div>
+    return <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><a href="/">Homepage</a></li>
+            <li><a href="/contacts">Contacts</a></li>
+            <li><Link to="/">Homepage</Link></li>
+            <li><Link to="/contacts">Contacts</Link></li>
+          </ul>
+        </nav>
+        <h1>{this.state.title}</h1>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/contacts" component={Contacts} />
+      </div>
+    </Router>
   }
 }
 

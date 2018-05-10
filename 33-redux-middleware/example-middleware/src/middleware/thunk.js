534e4aa416d2f9681608cefac773916e55be59ac
Steve Geluso <stevegeluso@gmail.com>
2018-05-09 19:39:57 -0700

    adding thunk examples

--- 33-redux-middleware/example-middleware/src/middleware/thunk.js
+++ 33-redux-middleware/example-middleware/src/middleware/thunk.js
@@ -1,3 +1,19 @@
+// if you pass a function in then thunk will simply call the function.
+// the function receives two parameters (dispatch, state) so it can
+// dispatch more functions itself in the future.
+// 
+// dispatch((dispatch, store) => {
+//   setInterval(() => dispatch(tickPerSecond), 1000);
+// })
+// 
+// import {showResults} from './actions/search-actions';
+// dispatch((dispatch, store) => {
+//   fetch('http://www.reddit.com/r/movies.json')
+//   .then(res => res.json())
+//   .then(json => {
+//     dispatch(showResults(json));
+//   });
+// })
 const thunk = store => next => action =>
   typeof action === 'function'
     ? action(store.dispatch, store.getState)

88345db24045f651c4b5c766aa1251d55dad20d2
Steve Geluso <stevegeluso@gmail.com>
2018-06-04 18:04:03 -0700

    initial socket

--- 43-realtime/demo/react-socker-server/index.js
+++ 43-realtime/demo/react-socker-server/index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+let clicks = 0;
+
+io.on('connection', socket => {
+  console.log('connected:', socket.id);
+  console.log('sending initial click info', clicks);
+  io.emit('click-info', {clicks});
+
+  socket.on('increment-click', () => {
+    console.log('got click');
+    clicks++;
+    console.log('broadcasting click', clicks);
+    io.emit('click-info', {clicks});
+  });
+});
 
 const Bundler = require('parcel-bundler');
 const bundler = new Bundler('./public/index.html');
 app.use(bundler.middleware());
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log('http://localhost:' + PORT);
 });
--- 43-realtime/demo/react-socker-server/public/src/app.js
+++ 43-realtime/demo/react-socker-server/public/src/app.js
@@ -1,15 +1,46 @@
 import React, {Component, Fragment} from 'react';
 import ReactDOM from 'react-dom';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:3000');
+socket.on('connect', () => {
+  console.log('connected');
+});
+
+socket.on('clicked', () => {
+  console.log('connected');
+});
 
 class App extends Component {
+  state = {
+    clicks: 0
+  }
+
+  componentDidMount() {
+    socket.on('click-info', (info) => {
+      console.log('got click info', info);
+      this.setState({clicks: info.clicks});
+    })
+  }
+
+  sendClick = () => {
+    console.log('client clicked');
+    socket.emit('increment-click');
+  }
+
   render() {
     return <Fragment>
       <h1>My App</h1>
       <p>Welcome to my app.</p>
+      <p>
+        <button onClick={this.sendClick}>click me</button>
+      </p>
+      <p>
+        Total clicks: {this.state.clicks}
+      </p>
     </Fragment>
   }
 }
 
-const root = document.createElement('div');
-document.body.appendChild(root);
+const root = document.getElementById('root');
 ReactDOM.render(<App/>, root);

1d86bc2349b250c9eaf3d5e0c8d1ec13bc89c11e
Steve Geluso <stevegeluso@gmail.com>
2018-06-06 21:24:11 -0700

    game ends when guesses go to zero

--- 44-software-architecture/lecture/public/src/components/spot-it/spot-it-engine.js
+++ 44-software-architecture/lecture/public/src/components/spot-it/spot-it-engine.js
@@ -1,13 +1,14 @@
 export default class SpotItEngine {
   constructor() {
-    this.card1 = null;
-    this.card2 = null;
-    this.createTwoCards();
+    this.isGameOver = false;
+    this.resetTwoCards();
   }
 
-  createTwoCards = () => {
+  resetTwoCards = () => {
     this.card1 = ["igloo", "dragon", "art", "lightning", "knight", "man", "dog", "padlock"];
     this.card2 = ["padlock", "lips", "anchor", "music-note", "flower", "exclamation-point", "car", "key"];
+    this.card1.sort(() => Math.random() < .5 ? -1 : 1);
+    this.card2.sort(() => Math.random() < .5 ? -1 : 1);
     this.guessesLeft = 3;
   }
 
@@ -16,19 +17,25 @@ export default class SpotItEngine {
       card1: this.card1,
       card2: this.card2,
       guessesLeft: this.guessesLeft,
+      isGameOver: this.isGameOver,
     }
   }
 
   isMatch = (symbol) => {
+    if (this.isGameOver) {
+      return;
+    }
+
     this.guessesLeft--;
+    if (this.guessesLeft === 0) {
+      this.isGameOver = true;
+    }
 
     const isInCard1 = this.card1.includes(symbol);
     const isInCard2 = this.card2.includes(symbol);
     const isMatch = isInCard1 && isInCard2;
     if (isMatch) {
-      let cards = [this.card1, this.card2];
-      this.card1 = cards[1];
-      this.card2 = cards[0];
+      this.resetTwoCards();
     }
     return isMatch;
   }
